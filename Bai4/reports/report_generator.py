336f0602f8382d0c41e440088a0b5e082594e250
HungNguyen254 <nguyenhung2540206@gmail.com>
2026-06-15 19:46:14 +0700

    Delete Bai4 directory

--- Bai4/reports/report_generator.py
+++
@@ -1,7 +0,0 @@
-from util.score_util import score_caculate
-def display_student(list):
-    count = 0
-    print('--Danh sách điểm sinh viên--')
-    for i in list:
-        avg_score,status =  score_caculate(i['scores'])
-        print(f'{count + 1}.[{i['student_id']}] {i['name']} | Điểm:{i['scores']} | ĐTB: {avg_score} - {status}')
